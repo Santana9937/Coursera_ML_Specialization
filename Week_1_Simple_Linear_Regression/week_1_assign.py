a5a82769e9ec031e10f3568107713337c9a006b9
Santana9937 <Santana9937@gmail.com>
2015-12-09 22:22:05 -0800

    Completed Question 1 for Quiz

--- Week_1_Simple_Linear_Regression/week_1_assign.py
+++ Week_1_Simple_Linear_Regression/week_1_assign.py
@@ -36,6 +36,46 @@ sales = pd.read_csv('kc_house_data.csv', dtype = dtype_dict)
 train_data = pd.read_csv('kc_house_train_data.csv', dtype = dtype_dict)
 test_data = pd.read_csv('kc_house_test_data.csv', dtype = dtype_dict)
 
+
+def simple_linear_regression(input_feature, output):
+
+	# Computing sums needed to calculate slope and intercept
+	xi_sum = sum(input_feature)
+	yi_sum = sum(output)
+	yi_xi_sum = sum(input_feature*output)
+	xi_squared_sum = sum(input_feature*input_feature)
+	N = float(len(input_feature))
+
+	# Values for slope and intercept
+	slope = (yi_xi_sum - (xi_sum*yi_sum)/N)/(xi_squared_sum - (xi_sum*xi_sum)/N)
+	intercept = yi_sum/N - slope*(xi_sum/N)
+
+	return (intercept, slope)
+
+test_feature = np.arange(5)
+test_output = 1.0 + 1.0*np.arange(5)
+(test_intercept, test_slope) =  simple_linear_regression(test_feature, test_output)
+print
+print "Intercept: " + str(test_intercept)
+print "Slope: " + str(test_slope)	
+
+sqft_intercept, sqft_slope = simple_linear_regression(train_data['sqft_living'].values, train_data['price'].values)
+print
+print "Intercept: " + str(sqft_intercept)
+print "Slope: " + str(sqft_slope)
+
+def get_regression_predictions(input_feature, intercept, slope):
+    
+    predicted_values = intercept + slope*input_feature
+
+    return predicted_values
+    
+my_house_sqft = 2650
+estimated_price = get_regression_predictions(my_house_sqft, sqft_intercept, sqft_slope)
+print
+print "The estimated price for a house with %d squarefeet is $%.2f" % (my_house_sqft, estimated_price)
+
+
 print
 print 'Done!'
 
